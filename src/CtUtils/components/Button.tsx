488c23ed1f71f88c511bb3f4e6169fbf10f91a2a
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-06-16 14:04:56 +0200

    add optional class names to button

--- src/CtUtils/components/Button.tsx
+++ src/CtUtils/components/Button.tsx
@@ -4,10 +4,11 @@ export const Button: FC<{
   title: string;
   action: () => void | Promise<void>;
   disabled?: boolean;
-}> = ({ title, action, disabled }) => {
+  className?: string;
+}> = ({ title, action, disabled, className }) => {
   return (
     <button
-      className="w-fit rounded-md border border-gray-900 cursor-pointer p-2 disabled:cursor-not-allowed disabled:bg-gray-400"
+      className={`w-fit rounded-md border border-gray-900 cursor-pointer p-2 disabled:cursor-not-allowed disabled:bg-gray-400${className ? ` ${className}` : ""}`}
       onClick={action}
       disabled={disabled}
     >
